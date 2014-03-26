0b0ce8e9e6391275cb742784583a3123d6efa893
Xiao Wang <wangxiao1254@gmail.com>
2014-03-26 09:54:31 -0400

    add fft

---
+++ src/circuits/FDFTLib.java
@@ -0,0 +1,80 @@
+package circuits;
+
+import flexsc.CompEnv;
+import gc.Signal;
+import objects.Float.Represention;
+//translated from https://www.ee.columbia.edu/~ronw/code/MEAPsoft/doc/html/FFT_8java-source.html
+
+public class FDFTLib extends FloatLib {
+
+	public FDFTLib(CompEnv<Signal> e) {
+		super(e);
+	}
+	
+	public void InverseFDFT(Represention[]x, Represention[] y) throws Exception {
+		FFT(x,y);
+		Represention invN = divide(publicFloat(1, x[0].v.length, x[0].p.length),
+				publicFloat(x.length, x[0].v.length, x[0].p.length)
+				);
+		for(int i = 0; i < x.length; ++i)
+			x[i] = multiply(invN, x[i]);
+		
+		for(int i = 0; i < y.length; ++i)
+			y[i] = multiply(invN, y[i]);
+	}
+	
+	public void FFT(Represention[] x, Represention[] y) throws Exception {
+		int lengthV = x[0].v.length, lengthP = x[0].p.length;;
+		int n = x.length;
+		int m = (int)(Math.log(n) / Math.log(2));
+		int n1;
+
+		// Bit-reverse
+		int j = 0;
+		int n2 = n/2;
+		for (int i = 1; i < n - 1; i++) {
+			n1 = n2;
+			while ( j >= n1 ) {
+				j = j - n1;
+				n1 = n1/2;
+			}
+			j = j + n1;
+
+			if (i < j) {
+				Represention t1 = x[i];
+				x[i] = x[j];
+				x[j] = t1;
+				t1 = y[i];
+				y[i] = y[j];
+				y[j] = t1;
+			}
+		}
+
+		// FFT
+		n1 = 0;
+		n2 = 1;
+
+		for (int i=0; i < m; i++) {
+			n1 = n2;
+			n2 = n2 + n2;
+			int a = 0;
+
+			for (j=0; j < n1; j++) {
+				Represention c = publicFloat(Math.cos(-2*Math.PI*a/n), lengthV, lengthP);
+				Represention s = publicFloat(Math.sin(-2*Math.PI*a/n), lengthV, lengthP);
+				a +=  1 << (m-i-1);
+
+				for (int k = j; k < n; k = k + n2) {
+					Represention t1 = sub(multiply(c,x[k+n1]), multiply(s, y[k+n1]));
+					Represention t2 = add(multiply(s,x[k+n1]), multiply(c, y[k+n1]));
+					
+					x[k+n1] = sub(x[k], t1);
+					y[k+n1] = sub(y[k], t2);
+					x[k] = add(x[k], t1);
+					y[k] = add(y[k], t2);
+				}
+			}
+		}
+		
+	}
+}
---
+++ src/test/TestFDFT.java
@@ -0,0 +1,143 @@
+package test;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+import objects.Float.Represention;
+import flexsc.CompEnv;
+import gc.GCEva;
+import gc.GCGen;
+import gc.Signal;
+
+import org.junit.Test;
+
+import circuits.FDFTLib;
+
+
+public class TestFDFT {
+	public abstract class Helper {
+		double[] a,b;
+		Helper(double[] a, double[] b) {
+			this.b = b;
+			this.a = a;
+		}
+		abstract void secureCompute(Represention[] a, Represention[] b, CompEnv<Signal> env) throws Exception;
+		abstract void plainCompute(double a[], double b[]);
+	}
+	
+	class GenRunnable extends network.Server implements Runnable {
+		Helper h;
+		double[] z1, z2;
+
+		GenRunnable (Helper h) {
+			this.h = h;
+		}
+
+		public void run() {
+			try {
+				listen(54321);
+
+				GCGen gen = new GCGen(is, os);
+				Represention[] fgc1 =new Represention[h.a.length];
+				Represention[] fgc2 =new Represention[h.b.length];
+				for(int i = 0; i < fgc1.length; ++i)
+					fgc1[i] = gen.inputOfGen(h.a[i], 10, 9);
+				for(int i = 0; i < fgc2.length; ++i)
+					fgc2[i] = gen.inputOfEva(10, 9);
+				h.secureCompute(fgc1, fgc2, gen);
+				os.flush();
+				
+				z1 = new double[fgc1.length];
+				z2 = new double[fgc2.length];
+				for(int i = 0; i < fgc1.length; ++i)
+					z1[i] = gen.outputToGen(fgc1[i]);
+
+				for(int i = 0; i < fgc2.length; ++i)
+					z2[i] = gen.outputToGen(fgc2[i]);
+
+				disconnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	class EvaRunnable extends network.Client implements Runnable {
+		Helper h;
+
+		EvaRunnable (Helper h) {
+			this.h = h;
+		}
+
+		public void run() {
+			try {
+				connect("localhost", 54321);	
+
+				GCEva eva = new GCEva(is, os);
+
+				Represention[] fgc1 = new Represention[h.a.length];
+				Represention[] fgc2 = new Represention[h.b.length];
+				for(int i = 0; i < fgc1.length; ++i)
+					fgc1[i] = eva.inputOfGen(10, 9);
+				
+				for(int i = 0; i < fgc2.length; ++i)
+					fgc2[i] = eva.inputOfEva(h.b[i], 10, 9);
+				
+				h.secureCompute(fgc1, fgc2, eva);
+				
+				for(int i = 0; i < fgc1.length; ++i)
+					eva.outputToGen(fgc1[i]);
+				for(int i = 0; i < fgc2.length; ++i)
+					eva.outputToGen(fgc2[i]);
+				os.flush();
+				disconnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	public void runThreads(Helper h) throws Exception {
+		GenRunnable gen = new GenRunnable(h);
+		EvaRunnable eva = new EvaRunnable(h);
+
+		Thread tGen = new Thread(gen);
+		Thread tEva = new Thread(eva);
+		tGen.start(); Thread.sleep(1);
+		tEva.start();
+		tGen.join();
+		
+		System.out.println(Arrays.toString(gen.z1));
+		
+		System.out.println(Arrays.toString(gen.z2));
+		
+		h.plainCompute(h.a, h.b);
+		System.out.println(Arrays.toString(h.a));
+		
+		System.out.println(Arrays.toString(h.b));
+	}
+	@Test
+	public void testAllCases() throws Exception {
+		Random rng = new Random();
+		int testCases = 1;
+
+		for (int i = 0; i < testCases; i++) {
+			runThreads(new Helper(new double[]{1, 1, 1, 1, 0, 0, 0, 0}, new double[]{0, 0, 0, 0, 0, 0, 0, 0}) {
+
+				@Override
+				void secureCompute(Represention[] a, Represention[] b,
+						CompEnv<Signal> env) throws Exception {
+					new FDFTLib(env).FFT(a, b);
+				}
+
+				@Override
+				void plainCompute(double[] a, double[] b) {
+					new FFT(8).fft(a, b);
+				}
+			});
+		}
+	}
+}
