82ca747e8cf82e551be75e68fca5516503b76f35
kartik <kartik@cs.umd.edu>
2014-06-11 13:45:11 -0700

    Test code files

---
+++ src/test/harness/Test_2input1outputPoint.java
@@ -0,0 +1,153 @@
+package test.harness;
+
+import ml.datastructure.Point;
+
+import org.junit.Assert;
+
+import pm.PMCompEnv;
+import test.Utils;
+import cv.CVCompEnv;
+import flexsc.CompEnv;
+import flexsc.Mode;
+import flexsc.Party;
+import gc.GCEva;
+import gc.GCGen;
+
+public class Test_2input1outputPoint<T> {
+	public abstract class Helper {
+		int dimension;
+		int width;
+		int[] intA, intB;
+		boolean[][] a;
+		boolean[][] b;
+		Mode m;
+
+		public Helper(int[] aa, int[] bb, Mode m, int dimension, int width) {
+			this.dimension = dimension;
+			this.width = width;
+			this.m = m;
+			intA = aa;
+			intB = bb;
+
+			a = new boolean[dimension][width];
+			b = new boolean[dimension][width];
+			for (int i = 0; i < dimension; i++) {
+				a[i] = Utils.fromInt(aa[i], 32);
+				b[i] = Utils.fromInt(bb[i], 32);
+			}
+		}
+
+		public abstract Point<T> secureCompute(Point<T> Signala, Point<T> Signalb, CompEnv<T> e) throws Exception;
+		public abstract int[] plainCompute(int[] x, int[] y);
+	}
+
+	class GenRunnable extends network.Server implements Runnable {
+		boolean[][] z;
+		Helper h;
+		GenRunnable (Helper h) {
+			this.h = h;
+		}
+
+		public void run() {
+			try {
+				listen(54321);
+
+
+				CompEnv<T> gen = null;
+				if(h.m == Mode.REAL)
+					gen = (CompEnv<T>) new GCGen(is, os);
+				else if(h.m == Mode.VERIFY)
+					gen = (CompEnv<T>) new CVCompEnv(is, os, Party.Alice);
+				else if(h.m == Mode.COUNT) 
+					gen = (CompEnv<T>) new PMCompEnv(is, os, Party.Alice);						
+				
+				
+				Point<T> a = new Point<T>(gen, 2);
+				Point<T> b = new Point<T>(gen, 2);
+				for (int i = 0; i < a.getDimension(); i++) {
+					a.coordinates[i] = gen.inputOfAlice(h.a[i]);
+					b.coordinates[i] = gen.inputOfBob(new boolean[32]);
+				}
+
+				Point<T> res = h.secureCompute(a, b, gen);
+				os.flush();
+
+				z = new boolean[a.getDimension()][];
+				for (int i = 0; i < a.getDimension(); i++) {
+					z[i] = gen.outputToAlice(res.coordinates[i]);
+				}
+
+				disconnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	class EvaRunnable extends network.Client implements Runnable {
+		Helper h;
+		EvaRunnable (Helper h) {
+			this.h = h;
+		}
+
+		public void run() {
+			try {
+				connect("localhost", 54321);				
+
+				CompEnv<T> eva = null;
+
+				if(h.m == Mode.REAL)
+					eva = (CompEnv<T>) new GCEva(is, os);
+				else if(h.m == Mode.VERIFY)
+					eva = (CompEnv<T>) new CVCompEnv(is ,os, Party.Bob);
+				else if (h.m == Mode.COUNT) 
+					eva = (CompEnv<T>) new PMCompEnv(is, os, Party.Bob);
+
+				Point<T> a = new Point<T>(eva, 2);
+				Point<T> b = new Point<T>(eva, 2);
+				for (int i = 0; i < a.getDimension(); i++) {
+					a.coordinates[i] = eva.inputOfAlice(new boolean[32]);
+					b.coordinates[i] = eva.inputOfBob(h.b[i]);
+				}
+
+				Point<T> res = h.secureCompute(a, b, eva);
+				os.flush();
+
+				// Point<Boolean> ungarbledRes = new Point<Boolean>(eva, 2);
+				boolean[][] ungarbledRes = new boolean[2][30];
+				for (int i = 0; i < a.getDimension(); i++) {
+					ungarbledRes[i] = eva.outputToAlice(res.coordinates[i]);
+					System.out.println(Utils.toInt(ungarbledRes[i]));
+				}
+
+				disconnect();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	public void runThreads(Helper h) throws Exception {
+		GenRunnable gen = new GenRunnable(h);
+		EvaRunnable eva = new EvaRunnable(h);
+		Thread tGen = new Thread(gen);
+		Thread tEva = new Thread(eva);
+		tGen.start(); Thread.sleep(5);
+		tEva.start();
+		tGen.join();
+		tEva.join();
+
+		//System.out.println(Arrays.toString(gen.z));
+		long[] z = new long[gen.z.length];
+		for (int i = 0; i < gen.z.length; i++) {
+			z[i] = Utils.toSignedInt(gen.z[i]);
+		}
+		int[] temp = h.plainCompute(h.intA, h.intB);
+		for (int i = 0; i < h.intA.length; i++) {
+			Assert.assertEquals(temp[i], z[i]);
+		}
+	}
+
+}
---
+++ src/test/ml/circuit/TestPointAdd.java
@@ -0,0 +1,38 @@
+package test.ml.circuit;
+
+import java.util.Random;
+
+import ml.circuit.PointLib;
+import ml.datastructure.Point;
+
+import org.junit.Test;
+
+import test.harness.Test_2input1outputPoint;
+import flexsc.CompEnv;
+import flexsc.Mode;
+
+public class TestPointAdd extends Test_2input1outputPoint<Boolean> {
+
+	@Test
+	public void testAllCases() throws Exception {
+		Random rnd = new Random();
+		int testCases = 1;
+
+		for (int i = 0; i < testCases; i++) {
+			int a[] = {3, 2};
+			int b[] = {7, 4};
+			Helper h = new Helper(a, b, Mode.VERIFY, 2 /* dimension */, 30 /* width */) {
+				public Point<Boolean> secureCompute(Point<Boolean> signalA, Point<Boolean> signalB, CompEnv<Boolean> e) throws Exception {
+					return new PointLib<Boolean>(e, 2, 30).add(signalA, signalB);}
+
+				public int[] plainCompute(int[] x, int[] y) {
+					for (int i = 0; i < x.length; i++) {
+						x[i] = x[i] + y[i];
+					}
+					return x;
+				}
+			};
+			runThreads(h);
+		}		
+	}
+}
