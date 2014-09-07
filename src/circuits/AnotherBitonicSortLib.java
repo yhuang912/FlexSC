b9192f2704821ac0bb4f56948354b83046e9175d
kartik <kartik@cs.umd.edu>
2014-09-07 19:59:53 -0400

    with payload added

--- src/circuits/AnotherBitonicSortLib.java
+++ src/circuits/AnotherBitonicSortLib.java
@@ -47,4 +47,48 @@ public class AnotherBitonicSortLib<T> extends IntegerLib<T> {
     	a[i] = ki;
     	a[j] = kj;
     }
+
+	public void sortWithPayload(T[][] a, T[][] data, T isAscending) {
+		this.isAscending = isAscending;
+		bitonicSortWithPayload(a, data, 0, a.length, isAscending);
+	}
+
+	private void bitonicSortWithPayload(T[][] a, T[][] data, int start, int n, T dir) {
+		if (n > 1) {
+			int m = n / 2;
+			bitonicSortWithPayload(a, data, start, m, isAscending);
+			bitonicSortWithPayload(a, data, start + m, n - m, not(isAscending));
+			bitonicMergeWithPayload(a, data, start, n, dir);
+		}
+	}
+
+	public void bitonicMergeWithPayload(T[][] a, T[][] data, int start, int n, T dir) {
+		if (n > 1) {
+			int m = compareAndSwapFirstWithPayload(a, data, start, n, dir);
+			bitonicMergeWithPayload(a, data, start, m, dir);
+			bitonicMergeWithPayload(a, data, start + m, n - m, dir);
+		}
+	}
+
+	public int compareAndSwapFirstWithPayload(T[][] a, T[][] data, int start, int n, T dir) {
+		int m = n / 2;
+		for (int i = start; i < start + m; i++) {
+			compareAndSwapWithPayload(a, data, i, i + m, dir);
+		}
+		return m;
+	}
+
+	private void compareAndSwapWithPayload(T[][] a, T[][] data, int i, int j, T dir) {
+    	T greater = not(leq(a[i], a[j]));
+    	T swap = eq(greater, dir);
+    	T[] ki = mux(a[i], a[j], swap);
+    	T[] kj = mux(a[j], a[i], swap);
+    	a[i] = ki;
+    	a[j] = kj;
+
+    	T[] di = mux(data[i], data[j], swap);
+    	T[] dj = mux(data[j], data[i], swap);
+    	data[i] = di;
+    	data[j] = dj;
+    }
 }
