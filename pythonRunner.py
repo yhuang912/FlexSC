c072d7f77d18e025d5ae29a3cab92c8016976117
kartik <kartik@cs.umd.edu>
2014-09-23 03:19:45 +0200

    mdoifying scripts to output to file

--- pythonRunner.py
+++ pythonRunner.py
@@ -2,9 +2,11 @@ import subprocess
 import os.path
 import time
 inputLength = 32
+subprocess.call("./clear_ports.sh", shell=True)
+subprocess.call("./kill_process.sh", shell=True)
 while (inputLength < 262144):
   for i in range(1, 2):
-    subprocess.call(["./start_process.sh /home/kartik/code/flexsc_final_workspace/first/bin " + str(1 << i) + " " + str(inputLength)], shell=True)
+    subprocess.call(["./start_process.sh /home/kartik/code/flexsc_final_workspace/first/bin " + str(1 << i) + " " + str(inputLength) + " out1.out"], shell=True)
     while(not os.path.isfile('mutex.txt')):
       time.sleep(60)
     os.remove('mutex.txt')
