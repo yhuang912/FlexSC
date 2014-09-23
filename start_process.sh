b942723c9c98d03289b80c1b269ca94529745f48
kartik <kartik@cs.umd.edu>
2014-09-23 03:11:21 +0200

    minor

--- start_process.sh
+++ start_process.sh
@@ -1,11 +1,13 @@
+#./clear_ports.sh
+#./kill_process.sh
 java network.Master 30000 50000 54321 true $2 $3 &
 java -cp .:$1/commons-io-2.4.jar network.Master 40000 55000 54321 false $2 $3 &
 for i in $(seq 0 $(($2 - 1)))
 do
-  java network.Machine $((30000 + $i )) $i $((35000 + $i )) true &
+  java network.Machine $((30000 + $i )) $i $((35000 + $i )) true $3 &
 done
 for i in $(seq 0 $(($2 - 1)))
 do
-  java -cp .:$1/commons-io-2.4.jar network.Machine $((40000 + $i )) $i $((35000 + $i )) false &
+  java -cp .:$1/commons-io-2.4.jar network.Machine $((40000 + $i )) $i $((35000 + $i )) false $3 &
 done
 
