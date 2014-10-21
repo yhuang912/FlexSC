c8cf7fc39cdea6f77b5af2cb56ed578e649377d4
kartik <kartik@cs.umd.edu>
2014-10-20 22:13:22 -0400

    runone.sh

--- src/test/parallel/ComputeHistogramResult.java
+++ src/test/parallel/ComputeHistogramResult.java
@@ -5,7 +5,7 @@ import flexsc.PMCompEnv.Statistics;
 public class ComputeHistogramResult {
 	static public void main(String args[]) throws InterruptedException {
 //		for (int logN = new Integer(args[0]); logN <= 20; logN+=new Integer(args[1])) {
-			Statistics oram = HistogramOram.getCount(256);
+			Statistics oram = HistogramOram.getCount(Integer.parseInt(args[0]));
 //			System.out.println(logN + "\t" + oram.andGate + "\t" + oram.NumEncAlice);
 //		}
 	}
