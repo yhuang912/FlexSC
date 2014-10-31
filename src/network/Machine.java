56e29f3eab5b7138a7fb4108b2fac33e08880093
kartik <kartik@cs.umd.edu>
2014-10-30 23:11:20 -0400

    sleep remvoed

--- src/network/Machine.java
+++ src/network/Machine.java
@@ -275,7 +275,7 @@ public class Machine {
 		machine.disconnect();
 
 		if (machine.machineId == firstPhysicalMachineId && (Mode.REAL.equals(machine.env.getMode()) || Party.Alice.equals(machine.env.party))) {
-			Thread.sleep(60000);
+			// Thread.sleep(60000);
 			PrintWriter writer = new PrintWriter("mutex.txt");
 			writer.close();
 		}
