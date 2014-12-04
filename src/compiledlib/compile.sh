20ff73254bba4ffb596e9aa8015ff26cb17be067
Xiao Wang <wangxiao1254@gmail.com>
2014-12-03 20:59:42 -0500

    .

---
+++ src/compiledlib/compile.sh
@@ -0,0 +1,1 @@
+java -cp ~/Documents/SCVM2/bin/ backend.flexsc.CommandTool $1 $2 $3
---
+++ src/compiledlib/stack/TestStackServer.java
@@ -0,0 +1,12 @@
+package compiledlib.stack;
+
+import compiledlib.stack.TestStack.GenRunnable;
+
+public class TestStackServer {
+
+	public  static void main(String args[]) throws Exception {
+
+			GenRunnable gen = new GenRunnable(20);
+			gen.run();
+	}
+}
