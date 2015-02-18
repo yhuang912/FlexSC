ea535c1b15272471bede5d1e239cf4343e0364c1
Ubuntu <ubuntu@ip-172-31-31-181.us-west-2.compute.internal>
2015-02-18 19:18:54 +0000

    .

--- test/oram/TestCircuitOramRecClient.java
+++ test/oram/TestCircuitOramRecClient.java
@@ -6,7 +6,7 @@ import flexsc.Flag;
 public class TestCircuitOramRecClient {
 
 	public  static void main(String args[]) throws Exception {
-		for(int i = 1; i <=26 ; i++) {
+		for(int i = 10; i <=26 ; i++) {
 			Flag.sw.flush();
 			EvaRunnable eva = new EvaRunnable(args[0], 54321);
 			eva.run();
--- test/oram/TestCircuitOramRecServer.java
+++ test/oram/TestCircuitOramRecServer.java
@@ -6,7 +6,7 @@ import flexsc.Flag;
 public class TestCircuitOramRecServer {
 
 	public  static void main(String args[]) throws Exception {
-		for(int i = 1; i <=26 ; i++) {
+		for(int i = 10; i <=26 ; i++) {
 			Flag.sw.flush();
 			GenRunnable gen = new GenRunnable(54321, i, 3, 1024-32, 8, 6);
 			gen.run();
