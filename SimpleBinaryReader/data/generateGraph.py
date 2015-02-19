ab0ba5bcf2a5372d4df9a600900066cafd8f2d2f
Allen McIntosh <amcintosh@appcomsci.com>
2015-02-19 17:35:17 -0500

    have many sizes in djikstra

--- SimpleBinaryReader/data/generateGraph.py
+++ SimpleBinaryReader/data/generateGraph.py
@@ -1,22 +1,28 @@
 import numpy 
 import sys
 
-graphSize = int(sys.argv[1])
+output = "{\n"
+for graphSize in range(3,20):
 
-array = numpy.random.random_integers(1, 40, (graphSize,graphSize))
+  array = numpy.random.random_integers(1, 40, (graphSize,graphSize))
 
-for i in range(0,graphSize):
+  for i in range(0,graphSize):
     array[i][i] = 0
     for j in range(i,graphSize):
         array[i][j] = array[j][i]
 
-output = "{"
-for i in range(0,graphSize):
+  if graphSize == 3:
+    output += "#if MAX == 3\n"
+  else :
+    output += "#elif MAX == " + str(graphSize) + "\n"
+  for i in range(0,graphSize):
     output += "{"
     for j in range(0,graphSize-1):
         output+= str(array[i][j]) + ","
     output+= str(array[i][graphSize-1])
     output += "},\n"
+
+output += "else\n\tWTF\n#endif\n"
 output += "};"
 
 print output
