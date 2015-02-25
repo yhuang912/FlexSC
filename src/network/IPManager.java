943be6592aa55c430dbd258af74ee04e1d3d2ec3
kartik <kartik@cs.umd.edu>
2015-02-24 20:23:28 -0500

    new ips for amazon

--- src/network/IPManager.java
+++ src/network/IPManager.java
@@ -33,6 +33,8 @@ public class IPManager {
 		ips[14] = "52.0.96.161"; // third_amazon_large virg
 		ips[15] = "54.153.7.55"; // cali
 		ips[16] = "50.18.217.125"; // cali 15-16 group
+		ips[17] = "54.153.48.156"; // cali 17-18 group
+		ips[18] = "54.153.34.218"; // cali 17-18 group
 		IPManager ipManager = new IPManager();
 		BufferedReader br = null;
 		try {
