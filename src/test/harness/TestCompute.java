83ff5de0814ceb2ab2ecdb9c58bd7bf6b4ffff5e
kartik <kartik@cs.umd.edu>
2015-02-26 02:10:19 -0500

    bw

--- src/test/harness/TestCompute.java
+++ src/test/harness/TestCompute.java
@@ -28,7 +28,7 @@ public class TestCompute extends TestHarness {
 		}
 		double t2 = System.nanoTime();
 		double t = (t2-t1)/1000000000.0;
-		System.out.println(ID + "\t" + t +"\t"+ 40894460.0/t);
+		System.out.println(ID + "\t" + t +"\t"+ 40894460.0 * 0.75 * 80 * 3/t);
 		return res;
 	}
 	int LEN = 4089446;
